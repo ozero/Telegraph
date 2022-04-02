b9e7bfb24a0e6f3648b944ade8313705478d836f
ozero_rgfx <rgfx0020@gmail.com>
2022-04-03 02:33:19 +0900

    update login.js

--- app/action/login.js
+++ app/action/login.js
@@ -17,3 +17,51 @@ const login = (req, res) => {
 
 module.exports = login;
 
+/*
+
+# Assume your Telegram bot authenticates & generates authorization url.
+
+example: telegraf.js
+
+- TELEGRAPH_SALT_1 is equal to process.env.SALT1
+- user_verified = [ok|ng]
+
+```
+let ctx = JSON.parse(event.body);
+let user_id = (ctx.message) ? ctx.message.chat.id : user_id;
+let user_name = (ctx.update) ? ctx.update.chat.username : user_name;
+
+bot.command('start', async (ctx) => {
+  let seed = ctx.message.text;
+  seed = (seed)?seed.replace("/start", "").replace(" ", ""):"";
+  console.log(['/start', ctx, seed]);
+
+  if(seed != ""){
+    let guest = true;
+
+    if(user_verified === "ok"){
+      guest = false;
+      let hash = sha256(user_data.user_id + seed + TELEGRAPH_SALT_1);
+      console.log(["Login:", user_data.user_id, seed, TELEGRAPH_SALT_1, hash]);
+      
+      //
+      let href = `https://[YOUR_TELEGRAPH_SERVER]/auth/${user_id}/${seed}/${hash}/${user_name}`;
+      ctx.reply(`You're authorized. Now you can open the article.\n`, {
+        parse_mode: 'HTML',
+        ...Markup.inlineKeyboard([ //button
+          Markup.button.url('Open article', href)//text, callback-key
+        ])
+      })
+    }
+
+    //
+    if(guest){
+      ctx.reply('Members only.');
+    }
+    return;
+  }
+});
+```
+
+
+*/

4c0904666027dd90719a3e16a9168368e052a3d4
ozero_rgfx <rgfx0020@gmail.com>
2022-04-03 14:18:42 +0900

    fix article_list

--- app/action/article_list.js
+++ app/action/article_list.js
@@ -6,8 +6,6 @@ const article_list = (req, res) => {
   let c = (!req.cookies) ? {} : req.cookies;
   let user_id = (c.user_id)?c.user_id:"";
   let username = (c.username)?c.username:"";
-  //
-  //console.log(['article_list', c.user_id, obj._doc]);
 
   article.find({ user_id: c.user_id }).sort([['date', -1]]).exec(function(err, obj) { 
     //console.log(['article_list', user_id, obj]);
@@ -16,31 +14,9 @@ const article_list = (req, res) => {
       article_user_id: user_id,
       article_user_name: username,
       site_title: process.env.SITE_TITLE,
-    });
-  
+    });  
 
   });
-
-
-  // article.find({ user_id: c.user_id }, (err, obj) => {
-  //   if ((obj === null)||(user_id === "")) {
-  //     console.log(['ERROR:article_list', url, err]);
-  //     res.render('article_list', {
-  //       list:[],
-  //       article_user_id: user_id,
-  //       article_user_name: username,
-  //     });
-
-  //   } else {
-  //     console.log(['article_list', user_id, obj]);
-  //     res.render('article_list', {
-  //       list:obj,
-  //       article_user_id: user_id,
-  //       article_user_name: username,
-  //     });
-
-  //   }
-  // });
 
 }
 
